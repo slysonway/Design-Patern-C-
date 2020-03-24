a205684df74aa8c98d3fb4090cf3a6e6bbb4092f
Pierre Kettmus <pkettmus@gmail.com>
2020-03-24 09:19:09 +0100

    add cheese

--- Kebab/Kebab.cs
+++ Kebab/Kebab.cs
@@ -19,15 +19,17 @@ namespace Kebab
         private bool onion { get; set; }
         private List<Meat> meat { get; set; }
         private List<Sauce> sauce { get; set; }
-        
+        private int cheese { get; set; }
 
-        public Kebab(bool salad, bool tomato, bool onion, List<Meat> meat, List<Sauce> sauce)
+
+        public Kebab(bool salad, bool tomato, bool onion, List<Meat> meat, List<Sauce> sauce, int cheese)
         {
             this.salad = salad;
             this.tomato = tomato;
             this.onion = onion;
             this.meat = meat;
             this.sauce = sauce;
+            this.cheese = cheese;
         }
         
         public bool isVegetarian()
@@ -38,6 +40,26 @@ namespace Kebab
         public bool isPescetarian()
         {
             return !meat.Contains(Meat.Chicken) && !meat.Contains(Meat.Steak) && !meat.Contains(Meat.Kebab);
+        }
+
+        public Kebab withOutOnion()
+        {
+            this.onion = false;
+            return this;
+        }
+
+        public Kebab addCheese()
+        {
+            if (cheese == 0)
+            {
+                cheese++;
+            }
+            else
+            {
+                cheese *= 2;
+            }
+            
+            return this;
         }
     }
 }
